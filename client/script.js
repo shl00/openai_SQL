e12e878eab50d874166cc773c8c62f6010882987
Sahil <sahillal25@outlook.de>
2023-06-20 15:42:15 +0200

    email validation

--- client/script.js
+++ client/script.js
@@ -2,25 +2,29 @@ import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
-let questions=[];
-let awnsers =[];
+let questions = [];
+let awnsers = [];
 let pCount = 0;
 let loadInterval;
 var name = "";
 var task = "";
-const taskNames =["TaskMoralDilemma","TaskHealthChat","TaskSQL","TaskTPT"];
+const taskNames = ["TaskMoralDilemma", "TaskHealthChat", "TaskSQL", "TaskTPT"];
 
-function isTask(){
-  for(let i = 0; i< taskNames.length;i++){
-    if(task.trim() === taskNames[i]){
-      return true
-    }
+function isValid(input){
+  const regEx =   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const string = input.toLowerCase();
+  if(string.match(regEx)){
+    return true;
   }
-  return false;
+  else{
+    alert("Keine gültige  E-Mail-Adresse!")
+    return false;
+  }
 }
-  name = window.prompt("Geben Sie Ihren Namen ein:");
-  //task = window.prompt("Task: " + "(" + taskNames[0]+","+taskNames[1] + " oder " + taskNames[2]+ ")");
-  task = taskNames[2];
+
+name = window.prompt("Geben Sie Ihre E-Mail-Adresse ein:");
+
+task = taskNames[3];
 function loader(element) {
   element.textContent = '';
 
@@ -71,107 +75,87 @@ function chatStripe(isAi, value, uniqueId) {
   )
 }
 
-const handleSubmit = async(e) =>{
+const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
 
-  chatContainer.innerHTML += chatStripe(false,data.get('promt'));
+  chatContainer.innerHTML += chatStripe(false, data.get('promt'));
   questions[pCount] = data.get('promt');
-  
+
   form.reset();
 
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true," ",uniqueId);
-  
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
   const messageDiv = document.getElementById(uniqueId);
-  
+
   loader(messageDiv);
   const input = name + " " + data.get('promt');
   //https://chatbot-pbxf.onrender.com
-  const response = await fetch('https://chatbot-sql.onrender.com',{
+  const response = await fetch('https://chatbot-tpt.onrender.com', {
     method: 'POST',
-    headers :{
+    headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      prompt:data.get('promt'),
-      na:name,
-      ta:taskNames[2],
+      prompt: data.get('promt'),
+      na: name,
+      ta: taskNames[3],
     })
-    
+
   })
   clearInterval(loadInterval);
   messageDiv.innerHTML = '';
-  if(response.ok){
+  if (response.ok) {
     const data = await response.json();
     console.log(data.bot)
     const parsedData = data.bot.content.trim();
     awnsers[pCount] = parsedData;
-    typeText(messageDiv,parsedData);
-  }else{
+    typeText(messageDiv, parsedData);
+  } else {
     const err = await response.text();
-    
+
     messageDiv.innerHTML = "Something went wrong";
 
     alert(err);
   }
-  
+
   pCount++;
 }
-function abcdjb(){
-  const date = new Date();
-  return  " " + date.getHours() + date.getFullYear() + date.getMonth() + date.getDay();
 
-}
-function sub(e){
-  console.log(name);
-  if(name.trim() != ""){
+
+form.addEventListener('submit', (e) => {
+  if (name.trim() != "" && isValid(name)) {
     handleSubmit(e);
   }
-  else{
-    name =window.prompt("Geben Sie Ihren Namen ein:");
-  }
-}
-form.addEventListener('submit',(e)=>{
-  if(name.trim() != "" && taskNames.includes(task)){
-      handleSubmit(e);
-  }
-  else{
-    if(name.trim() == ""){
-    name =window.prompt("Geben Sie Ihren Namen ein:");
+    else {
+      name = window.prompt("Geben Sie Ihre E-Mail-Adresse ein:");
     }
-    if(!taskNames.includes(task)){
-      task = window.prompt("Task: " + taskNames);
-    }
-  }
-});
-form.addEventListener('keyup', (e)=>{
-  if(e.keyCode === 13){
-  if(name.trim() != ""&& taskNames.includes(task)){
-   
-      handleSubmit(e);
-    }
-    else{
-      if(name.trim() == ""){
-        name =window.prompt("Geben Sie Ihren Namen ein:");
-        }
-        if(taskNames.includes(task)){
-          task = window.prompt("Task: " + taskNames);
-        }
-    }
-  }
-  
-});
+  });
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    if (name.trim() != "" && isValid(name) ) {
 
-const click = function(){
-  if(pCount>= 6){
-    alert("498946");
+      handleSubmit(e);
+    }
+      else{
+        name = window.prompt("Geben Sie Ihre E-Mail-Adresse ein:");
+      }
+    
+    }
   }
-  else{
+
+);
+
+const click = function () {
+  if (pCount >= 6) {
+    alert("");
+  }
+  else {
     alert("Mindestens 6 prompts!");
   }
 }
-document.getElementById("code").addEventListener('click',click);
+document.getElementById("code").addEventListener('click', click);
